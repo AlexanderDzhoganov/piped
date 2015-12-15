ca3d85cc9e7468e25af0e92a28826f456b612a70
AlexanderDzhoganov <alexander.dzhoganov@gmail.com>
2015-12-15 20:12:09 +0200

    0.0.6

--- test/main.mocha.js
+++ test/main.mocha.js
@@ -9,4 +9,90 @@ describe('rohr', function() {
             instance._object.foo.should.equal('bar');
         });
     });
+
+    describe('isString', function() {
+        it('test 1 (passing)', function() {
+            return rohr({foo: 'bar'})
+
+            .prop('foo').isString()
+
+            .toPromise();
+        });
+
+        it('test 2 (failing)', function() {
+            return rohr({foo: 1234})
+
+            .prop('foo').isString()
+
+            .toPromise().then(function() {
+                return Promise.reject();
+            }).catch(function() {
+                return Promise.resolve();
+            });
+        });
+    })
+
+    describe('rename', function() {
+        it('test 1', function() {
+            return rohr({foo: 'bar'})
+
+            .prop('foo').rename('baz')
+
+            .toPromise().then(function(object) {
+                object.baz.should.equal('bar');
+            });
+        })
+
+        it('test 2', function() {
+            return rohr({foo: 'bar'})
+
+            .prop('foo').transform(function(val) {
+                return 'hello';
+            }).rename('test')
+
+            .toPromise().then(function(object) {
+                object.test.should.equal('hello');
+            });
+        });
+    })
+
+    describe('transform', function() {
+        it('w/ synchronous return value', function() {
+            return rohr({foo: 'bar'})
+
+            .prop('foo').transform(function(val) {
+                return 'baz';
+            }).toPromise().then(function(object) {
+                object.foo.should.equal('baz');
+            });
+        });
+
+        it('w/ asynchronous return value', function() {
+            return rohr({foo: 'bar'})
+
+            .prop('foo').transform(function(val) {
+                return Promise.resolve('baz');
+            }).toPromise().then(function(object) {
+                object.foo.should.equal('baz');
+            });
+        });
+
+        it('w/ asynchronous return value #2', function() {
+            return rohr({foo: 'bar', test: 1234})
+
+            .prop('foo').isString().transform(function(val) {
+                return Promise.resolve('baz');
+            })
+
+            .prop('test').isNumber().transform(function(val) {
+                return Promise.resolve(val * 2);
+            })
+
+            .toPromise().then(function(object) {
+                object.foo.should.equal('baz');
+                object.test.should.equal(2468);
+            });
+        });
+
+    });
 });

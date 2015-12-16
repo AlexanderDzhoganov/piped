c1d0a3dede3262c8960e3b8f276e9f11db980214
AlexanderDzhoganov <alexander.dzhoganov@gmail.com>
2015-12-16 03:24:10 +0200

    0.2.0

--- test/rohr.internal.spec.js
+++ test/rohr.internal.spec.js
@@ -16,5 +16,40 @@ describe('rohr.internal', function() {
         });
     });
 
+    describe('_rootScope()', function() {
+        it('test #1', function() {
+            return rohr({ test: 2 })._rootScope().prop('test')
+
+            .toPromise().then(function(object) {
+                object.test.should.equal(2);
+            });
+        });
+
+        it('test #2', function() {
+            return rohr({ test: 2, foo: { bar: { baz: 1 } } })
+
+            .prop('foo').scope()
+                .prop('bar').scope()
+
+            ._rootScope().prop('test')
+
+            .toPromise().then(function(object) {
+                object.test.should.equal(2);
+            });
+        });
+    });
+
+    describe('_getScope()', function() {
+        it('test #1', function() {
+            return rohr({ test: 2 }).prop('test')
+            ._getScope().should.equal('test');
+        });
+
+        it('test #1', function() {
+            return rohr({ test: { foo: 'bar' } }).prop('test').scope()
+            ._getScope().should.equal('test');
+        });
+
+    });
 
 });
